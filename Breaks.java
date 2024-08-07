0a082443d40d4c177f21cb3959f1497cac1c1c8e
VarshiniManokaran <varshini6678@gmail.com>
2024-08-07 11:42:35 +0530

    Add files via upload

---
+++ Breaks.java
@@ -0,0 +1,19 @@
+public class Breaks {
+    public static void main(String[] args) {
+        
+        for(int i=4;i<50;i++){
+            
+            if(i == 3){
+                break;
+    
+            
+        }
+            System.out.println("i:"+i);
+            System.out.println("loop terminated");
+         
+
+    }
+    
+}
+}
+
